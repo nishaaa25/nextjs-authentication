6a0b6cdb1dacc22a046a2cd10214b3df7a78f714
nishaaa25 <sahnisha2505@gmail.com>
2025-02-12 22:34:07 +0530

    project-setup

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function UserDashboardPage() {
+    return <main className="w-screen min-h-screen overflow-x-hidden flex justify-center items-center flex-col">
+        <div className="w-4/12 text-bold relative">
+            DASHBOARD
+        </div>
+        <div className="w-8/12 relative">
+            hey
+        </div>
+    </main>
+};
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen overflow-x-hidden items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full ">
+        hey
+      </main>
+    </div>
+  );
+}
