25decc61221ecf65e2d866994da8749b324678cb
nishaaa25 <sahnisha2505@gmail.com>
2025-02-14 12:21:09 +0530

    created-auth-frm

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import AuthForm from "@/components/auth-form";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-screen overflow-x-hidden items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full ">
-        hey
+    <div className="w-screen overflow-x-hidden min-h-screen  font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full h-full  relative">
+        <AuthForm />
       </main>
     </div>
   );
---
+++ components/auth-form.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function AuthForm() {
+  return (
+    <div className="h-screen md:flex">
+      <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+        <div>
+          <h1 className="text-white font-bold text-4xl font-sans">GoFinance</h1>
+          <p className="text-white mt-1">
+            The most popular peer to peer lending at SEA
+          </p>
+          <button
+            type="submit"
+            className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+          >
+            Read More
+          </button>
+        </div>
+        <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+      </div>
+      <div className="flex md:w-1/2 justify-center py-10 items-center bg-white relative ">
+        <form className="bg-white w-1/2 relative">
+          <h1 className="text-gray-800 font-bold text-2xl mb-1">
+            Hello Again!
+          </h1>
+          <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <input
+              className="pl-2 outline-none border-none"
+              type="text"
+              name="fullname"
+              id="fullname"
+              placeholder="Full name"
+            />
+          </div>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <input
+              className="pl-2 outline-none border-none"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+            />
+          </div>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <input
+              className="pl-2 outline-none border-none"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+            <input
+              className="pl-2 outline-none border-none"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+          >
+            Sign Up
+          </button>
+          <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">
+            Don&apos;t have an account?
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+}
